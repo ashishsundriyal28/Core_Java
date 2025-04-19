d5f6526fec78aba592742f53082000782ee7a14e
Ashish Sundriyal <ashishsundriyal028@gmail.com>
2025-04-20 01:28:51 +0530

    CORE

---
+++ src/com/engineeringdigest/corejava/datatype/Main.java
@@ -0,0 +1,20 @@
+package com.engineeringdigest.corejava.datatype;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("sadjkfhsdjk");
+
+        // whole numbers
+            // byte
+            // short
+            // int
+            // Long
+        //decimal numbers
+            // float
+            // double
+        //characters
+            // char
+        //booleans
+            // boolean
+    }
+}
